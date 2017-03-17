5a629e056c7d3eb90621414b1fce6534531cf762
rajat <rajat.singh@ucalgary.ca>
2017-03-17 12:22:17 -0600

    Images and Json Response Enabled]

--- src/acquisition/RequestHandler.java
+++ src/acquisition/RequestHandler.java
@@ -1,13 +1,19 @@
 package acquisition;
+import java.io.InputStream;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.json.JSONObject;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import processing.DataRetriever; 
@@ -15,15 +21,42 @@ import processing.DataRetriever;
 @Path("/") 
 public class RequestHandler {
 
+//	@Path("")
+//	@GET
+//	@Produces("application/json")
+//	public Response HandleRequest (@Context UriInfo uriInfo) throws UnirestException {
+//
+//		//
+//		// TODO Calls to other modules
+//		JSONObject json = DataRetriever.FetchAurora(uriInfo);
+//		return Response.status(200).entity(json.toString()).build();
+//	}
+
 	@Path("")
 	@GET
-	@Produces("application/json")
-	public Response HandleRequest (@Context UriInfo uriInfo) throws UnirestException {
+	@Produces({"image/png","application/json"})
+	public Response HandleRequest2 (@Context UriInfo uriInfo) throws UnirestException {
 
 		String userQuery = uriInfo.getRequestUri().getQuery();
+		
+		if (userQuery.contains("type=embed")||userQuery.contains("type=images&image")){
+		HttpResponse<InputStream> response = 
+				   Unirest.get("http://api.auroras.live/v1/?"+userQuery)       
+				   .header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0")       
+				   .asBinary(); 
 		// TODO Calls to other modules
-		JSONObject json = DataRetriever.FetchAurora(userQuery);
-		return Response.status(200).entity(json.toString()).build();
+		//JSONObject json = DataRetriever.FetchAurora(uriInfo);
+		return Response.status(200).entity(response.getBody()).type("image/png").build();
+		}
+		else{
+			HttpResponse<JsonNode> response = 
+					   Unirest.get("http://api.auroras.live/v1/?"+userQuery)       
+					   .header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0")       
+					   .asJson();
+			JSONObject json = response.getBody().getObject();
+			json.put("attribution", "Powered by Auroras.live!");
+			return Response.status(200).entity(response.getBody().toString()).type("application/json").build();
+		}
 	}
 	
 }
--- src/processing/DataRetriever.java
+++ src/processing/DataRetriever.java
@@ -1,5 +1,7 @@
 package processing;
 
+import javax.ws.rs.core.UriInfo;
+
 import org.json.JSONException; 
 import org.json.JSONObject;
 import com.mashape.unirest.http.HttpResponse; 
@@ -9,16 +11,22 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class DataRetriever {  
 
-	public static JSONObject FetchAurora (String userQuery) throws JSONException, UnirestException {
-		
+	public static JSONObject FetchAurora (UriInfo uriInfo) throws JSONException, UnirestException {
+		JSONObject json = null;
+		String userQuery = uriInfo.getRequestUri().getQuery();
+		if (userQuery.contains("type=embed")||userQuery.contains("type=images&image")){
+			
+		}
+		else{
 		HttpResponse<JsonNode> response = 
 				   Unirest.get("http://api.auroras.live/v1/?"+userQuery)       
 				   .header("cookie", "PHPSESSID=MW2MMg7reEHx0vQPXaKen0")       
 				   .asJson();  
 		
-		JSONObject json = new JSONObject(response.getBody());
-		json.put("Attribution", "Powered by Auroras.live");
+		json = new JSONObject(response.getBody());
 		
+		json.put("Attribution", "Powered by Auroras.live");
+		}
 		return json;
 	}
 	
